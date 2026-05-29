2716c17b59f0d88c297d59b3ff8e9d092d647855
Ramiro <tupaparamon6@gmail.com>
2026-05-29 04:51:56 -0400

    creacion de la rutas y el controlador resource

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Producto;
+use App\Models\Categoria;
+use App\Http\Requests\ProductoRequest;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $productos = Producto::with('categoria')->get();
+        return view('productos.index', compact('productos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $categorias = Categoria::where('activo', true)->get();
+        return view('productos.create', compact('categorias'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProductoRequest $request)
+    {
+        Producto::create($request->validated());
+
+        return redirect()
+            ->route('productos.index')
+            ->with('success', 'Producto creado correctamente');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Producto $producto)
+    {
+        $producto->load('categoria');
+        return view('productos.show', compact('producto'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Producto $producto)
+    {
+        $categorias = Categoria::where('activo', true)->get();
+        return view('prductos.edit', compact('prodcuto', 'categorias'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ProductoRequest $request, Producto $producto)
+    {
+        $producto->update($request->validated());
+
+        return redirect()
+            ->route('productos.index')
+            ->with('success', 'Producto actualizado correctamente.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Producto $producto)
+    {
+        $producto->delete();
+
+        return redirect()
+            ->route('productos.index')
+            ->with('success', 'Producto eliminado correctamente.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('productos', ProductoController::class);
