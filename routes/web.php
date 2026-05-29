fc6d7f4a9bd093d0b408a934a3ac671c67f8f99c
Ramiro <tupaparamon6@gmail.com>
2026-05-29 08:44:05 -0400

    arreglo hacer que la pagina principal vaya a la ruta productos

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 
 Route::resource('productos', ProductoController::class);
+
+
+Route::get('/', function () {
+    return redirect()->route('productos.index');
+});
